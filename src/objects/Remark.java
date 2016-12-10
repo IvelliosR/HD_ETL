0b8415309436d70abe4337c5e84d9d4ff3a3dd27
IvelliosR <radek19941@gmail.com>
2016-12-10 21:26:11 +0100

    Add Remark.java

---
+++ src/objects/Remark.java
@@ -0,0 +1,25 @@
+package objects;
+
+public class Remark {
+	
+	private String name = "";
+	private String value = "";
+	
+	public Remark(String name, String value) {
+		this.name = name;
+		this.value = value;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
