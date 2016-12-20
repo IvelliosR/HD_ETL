3be4e5822e93c27697e5f0433ce1e27fe0dcf907
ablech <gargantua@interia.pl>
2016-12-20 21:57:28 +0100

    hibernate persistance framework added

--- src/main/java/pl/danych/hurtownie/test/Test.java
+++ src/main/java/pl/danych/hurtownie/test/Test.java
@@ -1,10 +1,19 @@
 package pl.danych.hurtownie.test;
 
+import pl.danych.hurtownie.objects.Comment;
+
 /**
  * Created by Arek on 20.12.2016.
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
+
+        Comment comment = new Comment();
+        comment.setAuthor("asdfasdf");
+        comment.setRate(1.23f);
+        comment.setSummary("very good");
+
+        System.out.println(comment);
     }
 }
